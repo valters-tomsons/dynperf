4343f664c01cd192a337f0e9b2afd7ec3cdc4273
Valters <infinity1188@yahoo.com>
2018-11-25 17:12:47 +0200

    Add targets file watcher and load new targets at runtime

--- dynperf-server/Domain/TargetProgramRepository.cs
+++ dynperf-server/Domain/TargetProgramRepository.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Linq;
 using dynperf_server.Models;
 using Newtonsoft.Json;
 
@@ -10,16 +12,13 @@ namespace dynperf_server.Domain
     {
         private readonly Configuration _configuration;
         private List<TargetProcessEntry> TargetProcessEntries { get; set; }
+        private FileSystemWatcher FileWatcher { get; set; }
 
         public TargetProgramRepository(Configuration configuration)
         {
             _configuration = configuration;
-            TargetProcessEntries = LoadTargetList(); 
-        }
-
-        public void Add(TargetProcessEntry entry)
-        {
-            TargetProcessEntries.Add(entry);
+            TargetProcessEntries = LoadTargetList();
+            InitializeFileWatcher();
         }
 
         public ReadOnlyCollection<TargetProcessEntry> GetEntries()
@@ -27,11 +26,29 @@ namespace dynperf_server.Domain
             return TargetProcessEntries.AsReadOnly();
         }
 
+        private void InitializeFileWatcher()
+        {
+            FileWatcher = new FileSystemWatcher();
+            FileWatcher.Path = Configuration.ConfigurationFolder;
+            FileWatcher.Filter = "targets.json";
+            FileWatcher.NotifyFilter = NotifyFilters.LastWrite;
+
+            FileWatcher.Changed += OnTargetsFileChange;
+            FileWatcher.EnableRaisingEvents = true;
+        }
+
+        private void OnTargetsFileChange(object sender, FileSystemEventArgs e)
+        {
+            System.Console.WriteLine("Targets file change detected, loading");
+            var newList = LoadTargetList();
+            TargetProcessEntries = newList;
+        }
+
         private List<TargetProcessEntry> LoadTargetList()
         {
             var targetsFile = _configuration.TargetListFilePath;
 
-            if(File.Exists(_configuration.TargetListFilePath))
+            if (File.Exists(_configuration.TargetListFilePath))
             {
                 return JsonConvert.DeserializeObject<List<TargetProcessEntry>>(File.ReadAllText(targetsFile));
             }
