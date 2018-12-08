b72b3d1f145def624f31086cbb628cdd5873b43c
Valters <infinity1188@yahoo.com>
2018-12-09 00:51:46 +0200

    Replace steam game definition entries with single Steam Overlay process

--- dynperf-server/Domain/DefaultTargetDefinition.cs
+++ dynperf-server/Domain/DefaultTargetDefinition.cs
@@ -8,12 +8,11 @@ namespace dynperf_server.Domain
         public static readonly List<TargetProcessEntry> DefaultsList = new List<TargetProcessEntry>()
         {
             new TargetProcessEntry("League of Legen") { Description = "League of Legends" },
-            new TargetProcessEntry("dota2") { Description = "Dota 2" },
             new TargetProcessEntry("Cemu") { Description = "Cemu (Wii U Emulator)"},
             new TargetProcessEntry("rpcs3") { Description = "RPCS3 (Playstation 3 Emulator)"},
             new TargetProcessEntry("retroarch") { Description = "Retroarch Emulator Frontend"},
             new TargetProcessEntry("Vainglory.exe") { Description = "Vainglory"},
-            new TargetProcessEntry("csgo_linux64") { Description = "Counter-Strike: Global Offensive"},
+            new TargetProcessEntry("gameoverlayui") { Description = "Steam In-Game Overlay"},
         };
     }
 }
